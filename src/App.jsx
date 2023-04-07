8e3f86f139c14a3beaf08eaa8eb70afb32a985c8
Firdosh01 <firdosh7392@gmail.com>
2023-04-07 07:47:32 -0500

    new project: Random gif

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Random from './Random';
+import Tag from './Tag';
+
+function App() {
+  return (
+    <div className="flex flex-col background">
+      <h1 className= "bg-white rounded-sm ">RANDOM GIFS</h1>
+
+      <div className="flex flex-col">
+        <Random />
+        <Tag />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
