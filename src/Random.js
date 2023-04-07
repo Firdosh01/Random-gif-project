a38f950be03edf57b867f784dc5d3f006d301857
Firdosh01 <firdosh7392@gmail.com>
2023-04-07 07:57:07 -0500

    gif basic structure

--- src/Random.js
+++ src/Random.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Random() {
+
+  const [gif, setGif] = useState('')
+
+  function clickHandler() {
+    
+  }
+
   return (
     <div>
-      
+      <h1>Random Gif</h1>
+      <img src={gif} width={400} />
+
+      <button onClick={clickHandler}>
+        Generate
+      </button>
     </div>
   )
 }
