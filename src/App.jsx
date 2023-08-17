0ca8c785ab4d7dbe5f3857208b06336f0c3a7c15
Firdosh01 <firdosh7392@gmail.com>
2023-08-16 23:44:01 -0500

    updated random gif

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Random from './Random';
-import Tag from './Tag';
+import Random from './components/Random';
+import Tag from './components/Tag';
 
 function App() {
   return (
-    <div className="flex flex-col background">
-      <h1 className= "heading">RANDOM GIFS</h1>
+    <div className="relative flex flex-col items-center w-full overflow-x-hidden background">
+      <h1 className= "bg-white rounded-lg w-11/12 text-center mt-[40px] px-10 py-2 text-4xl font-bold">RANDOM GIFS</h1>
 
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col w-full items-center gap-y-10 mt-[30px]">
         <Random />
         <Tag />
       </div>
@@ -18,3 +18,5 @@ function App() {
 }
 
 export default App;
+
+
